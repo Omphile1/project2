45eca9cf76c39379519395a730c2b51fe7981ec9
Omphile1 <53938132+Omphile1@users.noreply.github.com>
2020-11-24 07:43:31 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <div class="section"></div>
+      <div class="container"></div>
+        <simple-upload/>
+  </div>
+</template>
+<script>
+
+import SimpleUpload from './components/SimpleUpload';
+export default {
+  name:"App",
+  component:{SimpleUpload}
+}
+</script>
+
